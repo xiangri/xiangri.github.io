4a074567ebc24e3334f51a71378f6c638028176f
xiangri <xiangri1124@gmail.com>
2016-07-05 18:29:22 +0800

    Site updated: 2016-07-05 18:29:22

--- js/main.js
+++ js/main.js
@@ -67,7 +67,6 @@ require([], function (){
 				$(".article-inner .fancy-ctn").fancybox();
 			}
 		});
-		
 	}
 	//是否开启动画
 	if(yiliaConfig.animate === true){
@@ -79,7 +78,6 @@ require([], function (){
 				$(".js-avatar").addClass("show");
 			}
 		});
-		
 		if(yiliaConfig.isHome === true){
 			//content
 			function showArticle(){
@@ -99,12 +97,9 @@ require([], function (){
 			});
 			showArticle();
 		}
-		
 	}
-	
 	//是否新窗口打开链接
 	if(yiliaConfig.open_in_new == true){
 		$(".article a[href]").attr("target", "_blank")
 	}
-	
 });
